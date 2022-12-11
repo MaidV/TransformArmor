cf1e3b8d6832d58402620d92432e5a4febf24d86
MaidV <maid.v@protonmail.com>
2022-12-11 18:44:57 -0500

    Add G3B transforms

--- utils/build_maps.py
+++ utils/build_maps.py
@@ -8,20 +8,24 @@ import itertools
 with open('AllArmors_dump.json') as f:
     allarmor = json.load(f)
 
-def filter_armors(armor, prefix, slots):
-    return prefix.lower() in armor['name'].lower() and \
-        set(slots).intersection(set(armor['slots']))
+def filter_armors(armor, search, slots, prefix=False):
+    if prefix:
+        return armor['name'].lower().startswith(search.lower()) and \
+            set(slots).intersection(set(armor['slots']))
+    else:
+        return search.lower() in armor['name'].lower() and \
+            set(slots).intersection(set(armor['slots']))
 
-def get_armors(db, prefixes, excludes=None):
+def get_armors(db, search_strings, excludes=None, prefix=False):
     if not excludes:
         excludes = []
-    if isinstance(prefixes, str):
-        prefixes = [prefixes]
+    if isinstance(search_strings, str):
+        prefixes = [search_strings]
     
     armors = {}
     for mod in ['Skyrim.esm', 'Dawnguard.esm', 'Dragonborn.esm', 'Update.esm', 'Unofficial Skyrim Special Edition Patch.esp']:
-        for prefix in prefixes:
-            for armor in filter(lambda armor: filter_armors(armor, prefix, [32, 52]), db[mod].values()):
+        for string in search_strings:
+            for armor in filter(lambda armor: filter_armors(armor, string, [32, 52], prefix), db[mod].values()):
                 if armor['name'] not in excludes:
                     armors[mod + '|' + armor['formID']] = armor['name']
     return armors
@@ -63,40 +67,40 @@ armors['leather'] = get_armors(allarmor, ['Leather',
                                           "Studded Armor",
                                           "Studded Imperial Armor",
                                           "Scaled"])
-tops['leather'] = get_bikini_top(allarmor, 'Leather')
-bottoms['leather'] = get_bikini_bottom(allarmor, 'Leather')
+tops['leather'] = get_bikini_top(allarmor, ['Leather'])
+bottoms['leather'] = get_bikini_bottom(allarmor, ['Leather'])
 
 armors['iron'] = get_armors(allarmor, ['Iron', 'Banded Iron'])
-tops['iron'] = get_bikini_top(allarmor, 'Iron')
-bottoms['iron'] = get_bikini_bottom(allarmor, 'Iron')
+tops['iron'] = get_bikini_top(allarmor, ['Iron'])
+bottoms['iron'] = get_bikini_bottom(allarmor, ['Iron'])
 
 armors['steel'] = get_armors(allarmor, ['Steel Armor', 'Imperial Armor'], excludes=["Studded Imperial Armor"])
-tops['steel'] = get_bikini_top(allarmor, 'Steel Bikini')
+tops['steel'] = get_bikini_top(allarmor, ['Steel Bikini'])
 bottoms['steel'] = get_bikini_bottom(allarmor, 'Steel')
 
-armors['dwarven'] = get_armors(allarmor, 'Dwarven')
-tops['dwarven'] = get_bikini_top(allarmor, 'Dwarven Bikini')
-bottoms['dwarven'] = get_bikini_bottom(allarmor, 'Dwarven Bikini')
+armors['dwarven'] = get_armors(allarmor, ['Dwarven'])
+tops['dwarven'] = get_bikini_top(allarmor, ['Dwarven Bikini'])
+bottoms['dwarven'] = get_bikini_bottom(allarmor, ['Dwarven Bikini'])
 
-armors['ebony'] = get_armors(allarmor, 'Ebony')
-tops['ebony'] = get_bikini_top(allarmor, 'Ebony Bikini')
-bottoms['ebony'] = get_bikini_bottom(allarmor, 'Ebony')
+armors['ebony'] = get_armors(allarmor, ['Ebony'])
+tops['ebony'] = get_bikini_top(allarmor, ['Ebony Bikini'])
+bottoms['ebony'] = get_bikini_bottom(allarmor, ['Ebony'])
 
-armors['wolf'] = get_armors(allarmor, 'Wolf')
-tops['wolf'] = get_bikini_top(allarmor, 'Wolf')
-bottoms['wolf'] = get_bikini_bottom(allarmor, 'Wolf')
+armors['wolf'] = get_armors(allarmor, ['Wolf'])
+tops['wolf'] = get_bikini_top(allarmor, ['Wolf'])
+bottoms['wolf'] = get_bikini_bottom(allarmor, ['Wolf'])
 
-armors['orcish'] = get_armors(allarmor, 'Orcish')
-tops['orcish'] = get_bikini_top(allarmor, 'Orcish Bikini')
-bottoms['orcish'] = get_bikini_bottom(allarmor, 'Orcish')
+armors['orcish'] = get_armors(allarmor, ['Orcish'])
+tops['orcish'] = get_bikini_top(allarmor, ['Orcish Bikini'])
+bottoms['orcish'] = get_bikini_bottom(allarmor, ['Orcish'])
 
-armors['dragon'] = get_armors(allarmor, 'Dragonplate')
-tops['dragon'] = get_bikini_top(allarmor, 'Dragon Bone Bikini Top')
-bottoms['dragon'] = get_bikini_bottom(allarmor, 'Dragon Bone')
+armors['dragon'] = get_armors(allarmor, ['Dragonplate'])
+tops['dragon'] = get_bikini_top(allarmor, ['Dragon Bone Bikini Top'])
+bottoms['dragon'] = get_bikini_bottom(allarmor, ['Dragon Bone'])
 
-armors['nord'] = get_armors(allarmor, 'Nordic')
-tops['nord'] = get_bikini_top(allarmor, 'Nord Plate Bikini')
-bottoms['nord'] = get_bikini_bottom(allarmor, 'Nord Plate')
+armors['nord'] = get_armors(allarmor, ['Nordic'])
+tops['nord'] = get_bikini_top(allarmor, ['Nord Plate Bikini'])
+bottoms['nord'] = get_bikini_bottom(allarmor, ['Nord Plate'])
 
 transforms = {}
 for key in armors.keys():
@@ -105,9 +109,25 @@ for key in armors.keys():
     trg_bottoms = bottoms[key]
 
     for form,name in src_armors.items():
-        print(form, name)
         transforms[form] = {'32': list(trg_tops.keys()), '52': list(trg_bottoms.keys())}
-        
 
-with open('transforms.json', 'w', encoding ='utf8') as json_file:
-    json.dump(transforms, json_file, indent=4)
+with open('Vanilla-Bikini.json', 'w', encoding ='utf8') as json_file:
+    json.dump(transforms, json_file, indent=2)
+
+with open('AllArmors_dump.json') as f:
+    allarmor = json.load(f)
+
+G3Barmors = {}
+mod = 'Ghaan Revealing Outfit Craftable.esp'
+for armor in filter(lambda armor: filter_armors(armor, "G3B", [32, 52]), allarmor[mod].values()):
+    G3Barmors[mod + '|' + armor['formID']] = {"name": armor['name'], "slots": armor['slots']}
+
+g3btransforms = {}
+for key, val in G3Barmors.items():
+    basename = val['name'].split(maxsplit=1)[1]
+    print(basename)
+    for src_armor in get_armors(allarmor, [basename], prefix=True):
+        g3btransforms[src_armor] = {"32": [key,]}
+
+with open('Vanilla-G3B.json', 'w', encoding ='utf8') as json_file:
+    json.dump(g3btransforms, json_file, indent=2)
